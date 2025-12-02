bc5877d58a5f29e88a32c2ba467e5bb1d9d1e524
Philip <adegokephilip7@gmail.com>
2025-12-02 04:42:15 +0100

    Reapply "fixes"
    This reverts commit a73dbf8c29612d59e0d86803e3da68c2a1e84a6a.

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 interface PropertyProps {
     name: string;
     address: {
@@ -15,4 +16,22 @@ interface PropertyProps {
     };
     image: string;
     discount: string;
+=======
+import type { StaticImageData } from "next/image";
+
+export interface CardProps {
+    name: string;
+    address: string;
+    price: string;
+    ratings: string;
+    discount?: string;
+    image?: string | StaticImageData;
+}
+
+export interface ButtonProps {
+    name: string;
+    color: string;
+    type: "button" | "submit" | "reset";
+    onClick: () => void;
+>>>>>>> 340270f32cb35ab19303b1b7167d79f8b566a259
 }
