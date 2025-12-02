340270f32cb35ab19303b1b7167d79f8b566a259
Philip <adegokephilip7@gmail.com>
2025-12-02 02:55:59 +0100

    constant

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+// ---------- UI TEXT ----------
+export const UI_TEXT = {
+    submit: "Submit",
+    save: "Save",
+    cancel: "Cancel",
+    loading: "Loading...",
+    error: "Something went wrong",
+} as const;
